025e3b5e61021f2bf4c5c479b829ff4e9283c20f
Jie Dai <djay1128@yahoo.com>
2019-11-26 13:33:46 -0500

    fix a small issure

--- src/main/resources/static/resources/js/addCourse.js
+++ src/main/resources/static/resources/js/addCourse.js
@@ -30,5 +30,24 @@ let courseTemplate = "<div class=\"box-part text-center\">\n" +
     "                            </div>"
 
 courseAddBtn.click(function () {
-    addCourseInDashboard();
+    // addCourseInDashboard();
 });
+
+
+var button = document.getElementById("add-course-btn");
+var input = document.getElementById("Form-Course-Name");
+var ul = document.getElementById("course-group");
+// var ul = document.querySelector("ul");
+button.addEventListener("click", function() {
+    var li = document.createElement("li");
+    // Add Bootstrap class to the list element
+    // li.classList.add("list-group-item list-group-item-action list-group-item-secondary");
+    li.classList.add("list-group-item");
+    li.classList.add("list-group-item-action");
+    li.classList.add("list-group-item-secondary");
+
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    // Clear your input
+    input.value = "";
+})
