186cf0c74fff316c38c719784f6e09d91a640206
Valentina Pogudina <valentina.pogudina@stonybrook.edu>
2019-12-05 11:35:40 -0500

    modified Lab model

--- src/main/java/com/white/lab_sim/simulator/model/Lab.java
+++ src/main/java/com/white/lab_sim/simulator/model/Lab.java
@@ -7,6 +7,24 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Lab extends MarketUnit {
     private String Title;
     private String Description;
+    private List<Equipment> equipmentList;
+    private List<Step> stepList;
+
+    public List<Equipment> getEquipmentList() {
+        return equipmentList;
+    }
+
+    public void setEquipmentList(List<Equipment> equipmentList) {
+        this.equipmentList = equipmentList;
+    }
+
+    public List<Step> getStepList() {
+        return stepList;
+    }
+
+    public void setStepList(List<Step> stepList) {
+        this.stepList = stepList;
+    }
 
     public String getTitle() {
         return Title;
