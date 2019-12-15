eaebdcb4a8f4e12a02d6f81f969c671ee2f48f64
Jie Dai <djay1128@yahoo.com>
2019-12-14 23:30:00 -0500

    add pop-up dialog

--- src/main/resources/static/resources/js/market.js
+++ src/main/resources/static/resources/js/market.js
@@ -14,7 +14,7 @@ function courseFilterFunction() {
     }
 }
 
-
+// Move Items function
 $(function () { function moveItems(origin, dest) {
     $(origin).find(':selected').appendTo(dest);
 }
@@ -39,3 +39,88 @@ $(function () { function moveItems(origin, dest) {
         moveAllItems('#sbOne', '#sbTwo');
     });
 });
+
+
+// Add course function
+$( function() {
+    var dialog, form,
+        courseNamePatt = /^(?!\s*$).+/,
+        name = $( "#name" ),
+        section = $( "#section" ),
+        description = $( "#description" ),
+        allFields = $( [] ).add( name ).add( section ).add( description ),
+        tips = $( ".validateTips" );
+
+    function updateTips( t ) {
+        tips
+            .text( t )
+            .addClass( "ui-state-highlight" );
+        setTimeout(function() {
+            tips.removeClass( "ui-state-highlight", 1500 );
+        }, 500 );
+    }
+
+    function checkLength( o, n, min, max ) {
+        if ( o.val().length > max || o.val().length < min ) {
+            o.addClass( "ui-state-error" );
+            updateTips( "Length of " + n + " must be between " +
+                min + " and " + max + "." );
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function checkRegexp( o, regexp, n ) {
+        if ( !( regexp.test( o.val() ) ) ) {
+            o.addClass( "ui-state-error" );
+            updateTips( n );
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function addCourse() {
+        var valid = true;
+        allFields.removeClass( "ui-state-error" );
+
+        valid = valid && checkLength( name, "coursename", 3, 80 );
+
+        if ( valid ) {
+            $( "#users tbody" ).append( "<tr>" +
+                "<td>" + name.val() + "</td>" +
+                "<td>" + section.val() + "</td>" +
+                "<td>" + description.val() + "</td>" +
+                "</tr>" );
+            dialog.dialog( "close" );
+        }
+        return valid;
+    }
+
+    dialog = $( "#dialog-form" ).dialog({
+        autoOpen: false,
+        height: 400,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Create a course": addCourse,
+            Cancel: function() {
+                dialog.dialog( "close" );
+            }
+        },
+        close: function() {
+            form[ 0 ].reset();
+            allFields.removeClass( "ui-state-error" );
+        }
+    });
+
+    form = dialog.find( "form" ).on( "submit", function( event ) {
+        event.preventDefault();
+        addCourse();
+    });
+
+    $( "#create-course" ).button().on( "click", function() {
+        dialog.dialog( "open" );
+    });
+} );
