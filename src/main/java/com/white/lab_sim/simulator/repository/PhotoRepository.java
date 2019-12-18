382bc9c522c141c24053a9bbb13f197973e0415e
Valentina Pogudina <valentina.pogudina@stonybrook.edu>
2019-12-18 14:13:06 -0500

    dashboard can redirect to edit course page

--- src/main/java/com/white/lab_sim/simulator/repository/PhotoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.white.lab_sim.simulator.repository;
-
-import com.white.lab_sim.simulator.model.Photo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PhotoRepository extends MongoRepository<Photo, String> {
-
-}
